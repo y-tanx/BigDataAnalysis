7ffd53ad3cbca4998a157a7c1a35e3306ac3da5f
y-tanx <myc55945752@163.com>
2024-10-21 09:53:53 +0800

    Ready for lab2

---
+++ test/src/main/java/com/org/junitTest.java
@@ -0,0 +1,4 @@
+package com.org;
+
+public class junitTest {
+}
---
+++ test/src/test/java/com/org/junitTestTest.java
@@ -0,0 +1,8 @@
+package com.org;
+
+import junit.framework.TestCase;
+public class junitTestTest extends TestCase {
+
+    public void testAdd() {
+    }
+}
